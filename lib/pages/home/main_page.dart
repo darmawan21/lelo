5153f905d2f15873896c8fea8f5a4fb1d5e622a0
darmawan21 <d.darmawan2111@gmail.com>
2021-11-12 02:27:34 +0700

    add some page in bottom navbar

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lelo/pages/home/chat_page.dart';
+import 'package:lelo/pages/home/home_page.dart';
+import 'package:lelo/pages/home/plus_page.dart';
+import 'package:lelo/pages/home/profile_page.dart';
+import 'package:lelo/pages/home/wishlist_page.dart';
 import 'package:lelo/theme.dart';
 
 class MainPage extends StatefulWidget {
@@ -9,45 +14,89 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     Widget customBottomNav() {
       return BottomNavigationBar(
         backgroundColor: backgroundColor2,
+        currentIndex: currentIndex,
+        onTap: (value) {
+          setState(() {
+            print(value);
+            currentIndex = value;
+          });
+        },
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icon_home.png',
-              width: 21,
+            icon: Container(
+              margin: EdgeInsets.only(
+                top: 10,
+                bottom: 5,
+              ),
+              child: Image.asset(
+                'assets/icon_home.png',
+                width: 21,
+                color: currentIndex == 0 ? primaryColor : blackTextColor,
+              ),
             ),
             label: '',
           ),
           BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icon_buy.png',
-              width: 21,
+            icon: Container(
+              margin: EdgeInsets.only(
+                top: 10,
+                bottom: 5,
+              ),
+              child: Image.asset(
+                'assets/icon_buy.png',
+                width: 21,
+                color: currentIndex == 1 ? primaryColor : blackTextColor,
+              ),
             ),
             label: '',
           ),
           BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icon_plus.png',
-              width: 21,
+            icon: Container(
+              margin: EdgeInsets.only(
+                top: 10,
+                bottom: 5,
+              ),
+              child: Image.asset(
+                'assets/icon_plus.png',
+                width: 21,
+                color: currentIndex == 2 ? primaryColor : blackTextColor,
+              ),
             ),
             label: '',
           ),
           BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icon_chat.png',
-              width: 21,
+            icon: Container(
+              margin: EdgeInsets.only(
+                top: 10,
+                bottom: 5,
+              ),
+              child: Image.asset(
+                'assets/icon_chat.png',
+                width: 21,
+                color: currentIndex == 3 ? primaryColor : blackTextColor,
+              ),
             ),
             label: '',
           ),
           BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icon_profil.png',
-              width: 21,
+            icon: Container(
+              margin: EdgeInsets.only(
+                top: 10,
+                bottom: 5,
+              ),
+              child: Image.asset(
+                'assets/icon_profil.png',
+                width: 21,
+                color: currentIndex == 4 ? primaryColor : blackTextColor,
+              ),
             ),
             label: '',
           ),
@@ -55,12 +104,26 @@ class _MainPageState extends State<MainPage> {
       );
     }
 
+    Widget body() {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+        case 1:
+          return WishlistPage();
+        case 2:
+          return PlusPage();
+        case 3:
+          return ChatPage();
+        case 4:
+          return ProfilPage();
+        default:
+          return HomePage();
+      }
+    }
+
     return Scaffold(
-      backgroundColor: backgroundColor1,
-      bottomNavigationBar: customBottomNav(),
-      body: Center(
-        child: Text('Main'),
-      ),
-    );
+        backgroundColor: backgroundColor1,
+        bottomNavigationBar: customBottomNav(),
+        body: body());
   }
 }
