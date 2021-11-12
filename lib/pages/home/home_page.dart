acf744e855d62a7bb094b1532aabb5c18acece60
darmawan21 <d.darmawan2111@gmail.com>
2021-11-12 13:23:07 +0700

    add home page header

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,14 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:lelo/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget header() {
     return Container(
-      child: Center(
-        child: Text('Home Page'),
+      margin: EdgeInsets.only(
+        top: defaultMargin,
+        left: defaultMargin,
+        right: defaultMargin,
       ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Live Auctions',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: semibold,
+                  ),
+                ),
+                Text('For your collections',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 16,
+                      fontStyle: italic,
+                    )),
+              ],
+            ),
+          ),
+          Container(
+            width: 54,
+            height: 54,
+            child: Ink(
+              decoration: ShapeDecoration(
+                shape: CircleBorder(),
+                color: backgroundColor2,
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications_none,
+                  size: 32,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -36,3 +36,5 @@ FontWeight regular = FontWeight.w400;
 FontWeight medium = FontWeight.w500;
 FontWeight semibold = FontWeight.w600;
 FontWeight bold = FontWeight.w700;
+
+FontStyle italic = FontStyle.italic;
